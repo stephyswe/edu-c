487ed3990ebe067e40f7d8ab50d0c6a66e6a7356
Stephanie <stephaniiehallberg@gmail.com>
2023-01-28 17:01:40 +0100

    feat(01-01): first app complete

---
+++ 01-variables-n-datatypes-01-first-app/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+    // 2a
+    // printf("Hej jag heter Stephanie\n");
+    // printf("Testgatan 12\n");
+    // printf("111 55 Teststad\n");
+    //2b
+    printf("Hej jag heter Stephanie\nTestgatan 12\n111 55  Teststad\n");
+
+}
